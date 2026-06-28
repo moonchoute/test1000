5c8940d9f01c24081ee60dd0b52d515d6ed4867f
moonchoute <souhait.moon@gmail.com>
2026-06-28 16:47:50 +0900

    Hello World 파이썬 코드를 클래스 구조로 변경
    hello_world.py에 HelloWorldPrinter 클래스를 추가하고 출력 동작을 print_message 메서드로 캡슐화합니다.
    스크립트 실행 시 __main__ 블록에서 클래스 인스턴스를 생성해 기존과 동일하게 "Hello World"를 출력하도록 유지합니다.

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print("Hello World")
+class HelloWorldPrinter:
+    def print_message(self):
+        print("Hello World")
+
+
+if __name__ == "__main__":
+    printer = HelloWorldPrinter()
+    printer.print_message()
